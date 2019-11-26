9fdba259d4f7930e7eebc67cdf9a1b1144803782
YoonSuHyeon <96-05-03@naver.com>
2019-11-26 22:48:41 +0900

    2019-11-26 HomeActivity ,Find

--- app/src/main/java/com/mxn/soul/flowingdrawer/HomeActivity.java
+++ app/src/main/java/com/mxn/soul/flowingdrawer/HomeActivity.java
@@ -1,24 +1,34 @@
 package com.mxn.soul.flowingdrawer;
 
 import android.content.Intent;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import java.net.Inet4Address;
+import java.util.ArrayList;
 
 public class HomeActivity extends AppCompatActivity {
     private Button login,signup;
+    private ArrayList<Integer> imageList;
+    private static final int DP = 24; // 최대 저장 이미지수
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        login = findViewById(R.id.btn_login);
-        signup =findViewById(R.id.btn_signup);
-
+        login=findViewById(R.id.btn_login);
+        signup=findViewById(R.id.btn_signup);
+        this.initializeData();
+        ViewPager viewPager = findViewById(R.id.viewpager);
+        viewPager.setClipToPadding(false);
+        float density = getResources().getDisplayMetrics().density;
+        int margin = (int)(DP * density);
+        //viewPager.setPadding(margin,0,margin,0);
+        //viewPager.setPageMargin(margin/2);
+        viewPager.setAdapter(new PagerAdapter(this,imageList));
 
         login.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,12 +41,18 @@ public class HomeActivity extends AppCompatActivity {
         signup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String d ;
-
                 Intent signupintent = new Intent(HomeActivity.this,Sign_UpActivity.class);
                 startActivity(signupintent);
             }
         });
+    }
+
+    public void initializeData(){
+        imageList = new ArrayList();
+
+        imageList.add(R.drawable.image1);
+        imageList.add(R.drawable.image2);
+
 
     }
 }
