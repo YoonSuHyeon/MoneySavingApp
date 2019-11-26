62be38d1bfe42727522a029030e1a08a3cace559
YoonSuHyeon <96-05-03@naver.com>
2019-11-26 21:06:10 +0900

    2019-11-26  LoginActivity setOnclick

--- app/src/main/java/com/mxn/soul/flowingdrawer/LoginActivity.java
+++ app/src/main/java/com/mxn/soul/flowingdrawer/LoginActivity.java
@@ -11,16 +11,26 @@ import org.w3c.dom.Text;
 
 public class LoginActivity extends AppCompatActivity {
 private Button ok;
+private TextView find;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         ok=findViewById(R.id.btn_ok);
-        ok.setOnClickListener(new View.OnClickListener() {
+        find=findViewById(R.id.tv_find);
+
+        ok.setOnClickListener(new View.OnClickListener() {//확인 버튼을 눌렀을때
             @Override
             public void onClick(View view) {
                 Intent okintent = new Intent(LoginActivity.this,MainActivity.class);
                 startActivity(okintent);
+            }
+        });
+        find.setOnClickListener(new View.OnClickListener() { //아이디 비밀 번호 찾기 TextView 를 눌렀을때
+            @Override
+            public void onClick(View view) {
+                Intent findintent = new Intent(LoginActivity.this,Find_ID_PWActivity.class);
+                startActivity(findintent);
             }
         });
     }
