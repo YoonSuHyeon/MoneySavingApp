1478d4d97a2a8670bfcc30d6f09869d4068486ef
YoonSuHyeon <96-05-03@naver.com>
2019-11-26 18:27:59 +0900

    2019-11-26  Test

--- app/src/main/java/com/mxn/soul/flowingdrawer/HomeActivity.java
+++ app/src/main/java/com/mxn/soul/flowingdrawer/HomeActivity.java
@@ -1,25 +1,40 @@
 package com.mxn.soul.flowingdrawer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import java.net.Inet4Address;
+
 public class HomeActivity extends AppCompatActivity {
+    private Button login,signup;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        Button login = findViewById(R.id.btn_login);
-
+        login = findViewById(R.id.btn_login);
+        signup =findViewById(R.id.btn_signup);
 
 
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Intent loginintent = new Intent(HomeActivity.this,LoginActivity.class);
+                startActivity(loginintent);
             }
         });
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent signupintent = new Intent(HomeActivity.this,Sign_UpActivity.class);
+                startActivity(signupintent);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/mxn/soul/flowingdrawer/LoginActivity.java
+++ app/src/main/java/com/mxn/soul/flowingdrawer/LoginActivity.java
@@ -10,38 +10,11 @@ import android.widget.TextView;
 import org.w3c.dom.Text;
 
 public class LoginActivity extends AppCompatActivity {
-    Button loginButton;
-    TextView findIdTextView,sign_up_TextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        loginButton =findViewById(R.id.loginButton);
-        findIdTextView= findViewById(R.id.findIdTextView);
-        sign_up_TextView= findViewById(R.id.sign_up_TextView);
 
-        loginButton.setOnClickListener(new View.OnClickListener() { //Click loginButton
-            @Override
-            public void onClick(View view) {
-                Intent loginintent=new Intent(LoginActivity.this,MainActivity.class);
-                startActivity(loginintent);
-            }
-        });
-
-        findIdTextView.setOnClickListener(new View.OnClickListener() {//Click findIdTextView
-            @Override
-            public void onClick(View view) {
-                Intent findIdintent=new Intent(LoginActivity.this,Find_ID_PWActivity.class);
-                startActivity(findIdintent);
-            }
-        });
-
-        sign_up_TextView.setOnClickListener(new View.OnClickListener() {//Click sign_up_TextView
-            @Override
-            public void onClick(View view) {
-                Intent sign_upintent=new Intent(LoginActivity.this,Sign_UpActivity.class);
-                startActivity(sign_upintent);
-            }
-        });
     }
 }
