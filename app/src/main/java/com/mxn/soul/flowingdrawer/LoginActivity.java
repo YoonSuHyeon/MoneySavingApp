9b7adbee4ff8bd7f0da7fa200b62166d713219f6
YoonSuHyeon <96-05-03@naver.com>
2019-11-26 20:10:38 +0900

    2019-11-26  Firebase install error

--- app/src/main/java/com/mxn/soul/flowingdrawer/LoginActivity.java
+++ app/src/main/java/com/mxn/soul/flowingdrawer/LoginActivity.java
@@ -10,11 +10,18 @@ import android.widget.TextView;
 import org.w3c.dom.Text;
 
 public class LoginActivity extends AppCompatActivity {
-
+private Button ok;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
+        ok=findViewById(R.id.btn_ok);
+        ok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent okintent = new Intent(LoginActivity.this,MainActivity.class);
+                startActivity(okintent);
+            }
+        });
     }
 }
